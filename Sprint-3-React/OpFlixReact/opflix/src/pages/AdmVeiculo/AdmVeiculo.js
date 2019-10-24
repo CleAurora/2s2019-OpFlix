38f9d8620b2ff833b61d09ebfd4d1df54b419fc8
Cleís Aurora Silveira Pereira <34132336883@info.local>
2019-10-24 11:26:30 -0300

    Adiciona código para crude em veiculos - sem teste

--- Sprint-3-React/OpFlixReact/opflix/src/pages/AdmVeiculo/AdmVeiculo.js
+++ Sprint-3-React/OpFlixReact/opflix/src/pages/AdmVeiculo/AdmVeiculo.js
@@ -12,74 +12,163 @@ class AdmVeiculo extends Component {
     super();
     this.state = {
       lista: [],
-      nome: ''
+      listaVeiculosSelect: [];
+      nome: '',
+      editaNome: '',
+      idTipo: 0
     };
   }
 
-  
+  componentDidMount() {
+    this.listaTiposSelect()
+  }
+
   atualizaNome = (event) => {
     this.setState({ nome: event.target.value });
   }
 
-  listaveiculo = (event) =>{
-    event.preventDefault();
-    Axios.get('http://localhost:5000/api/veiculos', {
-      headers: {
-          "Content-Type": "application/json",
-          "Authorization": "Bearer " + localStorage.getItem("usuario-opflix")
-      }})
-    .then(resposta => {
-      if(resposta.status === 200){ 
-        let response = resposta.data;
-        this.setState({
-          lista: response
-        })
-      }else{
-        this.setState({ erro: "Oops! Tem erro.."})
-      }
-    })
-    .catch(erro =>{
-      this.setState({ erro: "Oops! Tem erro.."})
-    });
+  editarNome = (event) => {
+    this.setState({ editaNome: event.target.value });
   }
 
-  mudaParaTelaAdministrador = (event) =>{
+  editaIdTipo = (event) => {
+    this.setState({ idTipo: event.target.value })
+  }
+
+  mudaParaTelaAdministrador = (event) => {
     this.props.history.push('/administrador');
   }
 
-  cadastraInformacoes = (event) =>{
-    event.preventDefault();
-    Axios.post('http://localhost:5000/api/veiculos', {
-      nome: this.state.nome,
-    }, 
-    {
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + localStorage.getItem("usuario-opflix")
-      }
-    })
-    .then(response => {
-      if (response.status === 200) {
-        this.setState({
-          nome: ''
-        });
-      } else {
-        this.setState({ erro: 'Oops!' })
-      }
-    })
-    .catch(error => this.setState({ erro: "Oops! Tem erro.."})) 
-  }
-
-  logout = (event) =>{
+  logout = (event) => {
     localStorage.removeItem("usuario-opflix");
     localStorage.removeItem("isAdmin-opflix");
     this.props.history.push('/');
   }
 
+
+  //verbos http
+  listaVeiculosSelect() {
+    Axios.get('http://localhost:5000/api/veiculos', {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("usuario-opflix")
+      }
+    })
+      .then(response => {
+        if (response.status === 200) {
+          this.setState({ listaTiposSelect: response.data, lista: response.data });
+        }
+      });
+  }
+
+  listaveiculo = (event) => {
+    event.preventDefault();
+    Axios.get('http://localhost:5000/api/veiculos', {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("usuario-opflix")
+      }
+    })
+      .then(resposta => {
+        if (resposta.status === 200) {
+          let response = resposta.data;
+          this.setState({
+            lista: response,
+            listaTiposSelect: response
+          })
+        } else {
+          this.setState({ erro: "Oops! Tem erro.." })
+        }
+      })
+      .catch(erro => {
+        this.setState({ erro: "Oops! Tem erro.." })
+      });
+  }
+
+
+
+  cadastraInformacoes = (event) => {
+    event.preventDefault();
+    Axios.post('http://localhost:5000/api/veiculos', {
+      nome: this.state.nome,
+    },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + localStorage.getItem("usuario-opflix")
+        }
+      })
+      .then(response => {
+        if (response.status === 200) {
+          this.setState({
+            nome: '',
+            erro: '',
+            lista: []
+          });
+        } else {
+          this.setState({ erro: 'Oops!' })
+        }
+      })
+      .catch(error => this.setState({ erro: "Oops! Tem erro.." }))
+  }
+
+  alteraInformacoes = (event) => {
+    event.preventDefault();
+
+    Axios.put('http://localhost:5000/api/veiculos/' + this.state.idTipo,
+      {
+        nome: this.state.editaNome
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + localStorage.getItem("usuario-opflix")
+        }
+      })
+      .then(response => {
+        if (response.status === 200) {
+          this.setState({
+            editaNome: '',
+            idVeiculo: '0'
+          });
+          this.listaTiposSelect();
+        } else {
+          this.setState({ erro: 'Oops!' })
+        }
+      })
+      .catch(error => this.setState({ erro: 'Falha ao tentar atualizar tipo!' }))
+  }
+
+  deletaTipo = (event) => {
+    event.preventDefault();
+
+    Axios.delete('http://localhost:5000/api/veiculos/' + this.state.idTipo,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + localStorage.getItem("usuario-opflix")
+        }
+      })
+      .then(response => {
+        if (response.status === 204) {
+          this.setState({
+            editaNome: '',
+            idVeiculo: "0",
+            erro: ''
+          });
+          this.listaVeiculosSelect();
+        } else {
+          this.setState({ erro: 'Oops!' })
+        }
+      })
+      .catch(error => this.setState({ erro: 'Falha ao tentar deletar tipo!' }))
+  }
+
+
   render() {
     return (
       <div>
-        <Header funcao={this.logout}/>
+        <Header funcao={this.logout} />
         <main className="conteudoPrincipal">
           <section className="conteudoPrincipalAdministrador">
             <h2>Veículo</h2>
@@ -110,26 +199,58 @@ class AdmVeiculo extends Component {
               }
             </tbody>
           </table>
-          
+
           <div className="container">
-          <input type="text"
-            placeholder="Digite o Veículo"
-            onChange={this.atualizaNome}
-            value={this.state.nome}
-          />
+            <input type="text"
+              placeholder="Digite o Veículo"
+              onChange={this.atualizaNome}
+              value={this.state.nome}
+            />
 
-          <p hidden={this.state.erro === ''}
-            style={{ color: "red", textAlign: "center" }}
-          >
-            {this.state.erro}
-          </p>
+            <p hidden={this.state.erro === ''}
+              style={{ color: "red", textAlign: "center" }}
+            >
+              {this.state.erro}
+            </p>
 
-          <button
-            className="conteudoPrincipal-btn"
-            onClick={this.cadastraInformacoes}
-          >
-            Cadastrar
-          </button>
+            <button
+              className="conteudoPrincipal-btn"
+              onClick={this.cadastraInformacoes}
+            >
+              Cadastrar
+            </button>
+
+            <h3>Para Deletar e Alterar selecione a opção</h3>
+
+            <select id="option" onChange={this.editaIdTipo} value={this.state.idTipo}>
+              <option value="0" disabled >Selecione o veículo a ser alterado ou deletado</option>
+              {this.state.listaVeiculosSelect.map(element => {
+                return (
+                  <option
+                    value={element.idVeiculo}
+                    key={element.idVeiculo}
+                  >
+                    {element.nome}
+                  </option>
+                )
+              })}
+            </select>
+
+            <input type="text"
+              placeholder="Digite o veiculo a ser alterado"
+              value={this.state.editaNome}
+              onChange={this.editarNome}
+            />
+            <button className="conteudoPrincipal-btn" onClick={this.alteraInformacoes}>Alterar Informações</button>
+            <button className="conteudoPrincipal-btn" onClick={this.deletaVeiculo}>Deletar Veiculo</button>
+
+            <p hidden={this.state.erro === ''}
+              style={{ color: "red", textAlign: "center" }}
+            >
+              {this.state.erro}
+            </p>
+
+
           </div>
 
           <img src={telaFundo} alt="Família vendo tv" className="telaFundo" />
