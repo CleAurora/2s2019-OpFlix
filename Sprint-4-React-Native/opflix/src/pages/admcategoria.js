7cb57655068a9e6595c4745c802372f3c1121495
Cleís Aurora Silveira Pereira <34132336883@info.local>
2019-11-18 12:22:58 -0200

    Adiciona cadastrar e começa atualizar em categoria

--- Sprint-4-React-Native/opflix/src/pages/admcategoria.js
+++ Sprint-4-React-Native/opflix/src/pages/admcategoria.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, Image, StyleSheet, FlatList, AsyncStorage, ImageBackground } from 'react-native';
+import { Text, View, Image, StyleSheet, FlatList, AsyncStorage, ImageBackground, TouchableOpacity, TextInput, Picker } from 'react-native';
 import planoDeFundo from '../assets/img/familia-vendo-tv1.jpg'
 import menu from '../assets/img/menuhamburger.png'
 import Axios from 'axios';
@@ -31,9 +31,9 @@ class Categorias extends Component {
     this._carregarCategorias();
   }
 
-  atualizaNome = (event) => {
-    this.setState({ nome: event.target.value });
-  }
+  // atualizaNome = (event) => {
+  //   this.setState({ nome: event.target.value });
+  // }
 
   atualizaNomeASerAlterado = (event) => {
     this.setState({ nomeASerAlterado: event.target.value });
@@ -56,71 +56,46 @@ class Categorias extends Component {
       .catch(erro => console.warn(erro));
   };
 
-  _cadastraCategorias = async (event) => {
-    await Axios.post('http://192.168.3.192:5000/api/categorias', {
-      headers: {
-        "Content-Type": "application/json",
-        'Authorization': 'Bearer ' + await AsyncStorage.getItem('@opflix:token')
-      }
-    })
-      .then(resposta => {
-        if (resposta.status === 200) {
-          this.setState({ categorias: [], nome: '' });
-        } else {
-          this.setState({ erro: "Oops! Tem erro.." })
-        }
-      })
-      .catch(erro => { this.setState({ erro: "Falha ao tentar cadastrar" }) });
-  }
+  _cadastraCategorias = async () => {
 
-  _atualizaCategoria = async (event) => {
-    await Axios.put('http://192.168.3.192:5000/api/categorias' + this.state.idCategoria, {
-      nome: this.state.nomeASerAlterado
+    await Axios.post('http://192.168.3.192:5000/api/categorias', {
+      "nome": this.state.nome,
     },
       {
         headers: {
-          "Content-Type": "application/json",
-          "Authorization": "Bearer " + await AsyncStorage.getItem('@opflix:token')
+          'Authorization': 'Bearer ' + await AsyncStorage.getItem('@opflix:token')
         }
       })
       .then(response => {
         if (response.status === 200) {
           this.setState({
-            nomeASerAlterado: '',
-            idCategoria: '0'
+            categorias: [],
+            nome: ''
           });
-          this.categorias();
+          this._carregarCategorias();
         } else {
           this.setState({ erro: 'Oops!' })
         }
       })
-      .catch(error => this.setState({ erro: 'Falha ao tentar atualizar categoria!' }))
+      .catch(error => this.setState({ erro: 'Falha ao tentar cadastrar categoria!' }))
+
   }
 
-  _deletaCategoria = async (event) => {
-    await Axios.delete('http://192.168.3.192:5000/api/categorias' + this.state.idCategoria,
+  // rever atualizar - parei aqui
+  _atualizaCategoria = async () => {
+    console.warn(this.state.idCategoria);
+    console.warn(await AsyncStorage.getItem('@opflix:token'))
+
+    await Axios.put('http://192.168.3.192:5000/api/categorias', {
+      "idCategoria": this.state.idCategoria,
+    },
       {
         headers: {
-          "Content-Type": "application/json",
-          "Authorization": "Bearer " + await AsyncStorage.getItem('@opflix:token')
+          'Authorization': 'Bearer ' + await AsyncStorage.getItem('@opflix:token')
         }
-          .then(response => {
-            if (response.status === 204) {
-              this.setState({
-                nomeASerAlterado: '',
-                idCategoria: "0",
-                erro: ''
-              });
-              this.categorias();
-            } else {
-              this.setState({ erro: 'Falha ao tentar deletar categoria!' })
-            }
-          })
-          .catch(error => this.setState({ erro: 'Não é possível deletar uma categoria que possui lançamento associado!' }))
-
       })
-
   }
+
 
   render() {
     return (
@@ -147,23 +122,67 @@ class Categorias extends Component {
             )}
           />
 
-          <Text style={styles.tittleText}>Atualiza Categorias</Text>
           <View>
-            <input
-              placeholder="Digite a Categoria"
-              onChange={this.atualizaNome}
+            <Text style={styles.tittleText}>Cadastra Categorias</Text>
+          </View>
+          <View style={styles.formularioArea}>
+            <TextInput
+              style={styles.inputArea}
+              placeholder="categoria"
+              onChangeText={nome => this.setState({ nome })}
               value={this.state.nome}
             />
-
-            <button
-              onClick={this._cadastraCategorias}
+            <TouchableOpacity
+              onPress={this._cadastraCategorias}
+              style={styles.btn}
             >
-              Cadastrar
-            </button>
+              <Text style={styles.textTittle}>Cadastrar</Text>
+            </TouchableOpacity>
           </View>
 
-         
-          />
+          <View>
+            <Picker
+              style={styles.tittleText}
+              onChangeText={this.atualizaIdCategoria}
+              value={this.state.idCategoria}
+
+            >
+              {this.state.categorias.map(element => {
+                return (
+                  <Picker.Item
+                    value={element.idCategoria}
+                    label={element.nome}
+                  />
+                )
+              })}
+
+            </Picker>
+          </View>
+
+          <View style={styles.formularioArea}>
+            <TextInput
+              style={styles.inputArea}
+              placeholder="Categoria a ser alterada"
+              value={this.state.nomeASerAlterado}
+              onChangeText={this.atualizaNomeASerAlterado}
+            />
+            <TouchableOpacity
+              onPress={this._atualizaCategoria}
+              style={styles.btn}
+            >
+              <Text style={styles.textTittle}>Atualizar Categoria</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={this._deletaCategoria}
+              style={styles.btn}
+            >
+              <Text style={styles.textTittle}>Deletar Categoria</Text>
+            </TouchableOpacity>
+
+          </View>
+
+
+
 
         </ImageBackground>
       </View>
@@ -177,9 +196,35 @@ const styles = StyleSheet.create({
     height: 35
   },
 
+  textTittle: {
+    fontFamily: 'Cohin',
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    textAlign: 'center',
+  },
+
+
   baseText: {
     fontFamily: 'Cohin',
     padding: 10,
+    fontSize: 20,
+  },
+
+  inputArea: {
+    backgroundColor: '#ffffff',
+    textAlign: 'center'
+  },
+
+  formularioArea: {
+    backgroundColor: 'rgba(72, 68, 69, 0.5)',
+    marginHorizontal: '10%',
+
+  },
+
+  btn: {
+    backgroundColor: '#484445',
+    height: 30
   },
 
   tittleText: {
