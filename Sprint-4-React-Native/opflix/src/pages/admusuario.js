e801cbbc149e0302389fccceaf207841c219aa88
Cleís Aurora Silveira Pereira <34132336883@info.local>
2019-11-29 08:59:16 -0200

    adiciona layout usuários

--- Sprint-4-React-Native/opflix/src/pages/admusuario.js
+++ Sprint-4-React-Native/opflix/src/pages/admusuario.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, Image, StyleSheet, FlatList, AsyncStorage, ImageBackground } from 'react-native';
+import { Text, View, Image, StyleSheet, FlatList, AsyncStorage, ScrollView, ImageBackground } from 'react-native';
 import planoDeFundo from '../assets/img/familia-vendo-tv1.jpg'
 import menu from '../assets/img/menuhamburger.png'
 
@@ -41,31 +41,38 @@ class Usuarios extends Component {
       <View>
         <ImageBackground
           source={planoDeFundo}
-          style={{width:"100%", height: "100%"}}
+          style={{ width: "100%", height: "100%" }}
         >
           <View style={styles.headerArea}>
             <Image
               source={menu}
-              style={{ width: "5%", height: "16%", tintColor: "#fff" }}></Image>
+              style={{ width: 40, height: 40, resizeMode: "stretch", marginBottom: "-7%", marginTop: "1%", marginLeft: "0.5%", tintColor: '#ffffff' }}></Image>
             <Text style={styles.textTittle}>OPFLIX</Text>
           </View>
 
-          <Text style={styles.tittleText}>Lista de Usuários</Text>
-          <FlatList
-            style={styles.Text}
-            data={this.state.usuarios}
-            keyExtractor={item => item.idUsuario}
-            renderItem={({ item }) => (
-              <View style={styles.body}>
-                <Text style={styles.text}>{item.nome}</Text>
-                <Text style={styles.text}>{item.email}</Text>
-                <Text style={styles.text}>{item.celular}</Text>
-                <Text style={styles.text}>{item.endereco}</Text>
-                <Text style={styles.text}>{item.idPerfil}</Text>
-                <Text></Text>
-              </View>
-            )}
-          />
+
+          <ScrollView>
+            <Text></Text>
+            <Text></Text>
+            <View style={styles.caixaBranca1}>
+              <Text style={styles.tittleText1}>Lista de Usuarios</Text>
+              <FlatList
+                style={styles.Text}
+                data={this.state.usuarios}
+                keyExtractor={item => item.idUsuario}
+                renderItem={({ item }) => (
+                  <View>
+                    <Text style={styles.text}>{item.nome}</Text>
+                    <Text style={styles.text}>{item.email}</Text>
+                    <Text style={styles.text}>{item.celular}</Text>
+                    <Text style={styles.text}>{item.endereco}</Text>
+                    <Text style={styles.text}>{item.idPerfil}</Text>
+                    <Text></Text>
+                  </View>
+                )}
+              />
+            </View>
+          </ScrollView>
         </ImageBackground>
       </View>
     );
@@ -79,9 +86,34 @@ const styles = StyleSheet.create({
     height: 35
   },
 
+  areaTittle: {
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+  },
+
   baseText: {
     fontFamily: 'Cohin',
     padding: 10,
+    fontSize: 20,
+  },
+
+  inputArea: {
+    backgroundColor: '#ffffff',
+    textAlign: 'center'
+  },
+
+  formularioArea: {
+    marginHorizontal: '10%',
+
+  },
+
+  btn: {
+    backgroundColor: '#484445',
+    height: 25
+  },
+
+  picker: {
+    backgroundColor: "#FFF",
+    margin: "10%"
   },
 
   tittleText: {
@@ -93,19 +125,60 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 
-  text: { color: 'white', fontSize: 15 },
-  body: { backgroundColor: 'rgba(72, 68, 69, 0.8)'  },
-  headerArea:{
+  tittleText1: {
+    fontFamily: 'Cohin',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#484445',
+    padding: 16,
+    alignItems: 'center',
+  },
+
+  text: {
+    color: 'black', fontSize: 15, textAlign: "center"
+  },
+
+  body: { backgroundColor: 'rgba(72, 68, 69, 0.8)' },
+
+  headerArea: {
     backgroundColor: '#484445',
+  },
+
+  feedback: {
+    fontSize: 15,
+    textAlign: "center",
+    marginBottom: 10
   },
 
   textTittle: {
     fontFamily: 'Cohin',
-    fontSize: 25,
+    fontSize: 30,
+    marginTop: "-3%",
+    marginBottom: "1%",
+    fontWeight: 'bold',
+    color: '#ffffff',
+    textAlign: 'center',
+  },
+
+  textTittlebtn: {
+    fontFamily: 'Cohin',
+    fontSize: 16,
     fontWeight: 'bold',
     color: '#ffffff',
     textAlign: 'center',
     marginBottom: -50,
+  },
+  caixaBranca: {
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    paddingBottom: "6%",
+    marginHorizontal: "10%",
+    marginVertical: "5%",
+    borderRadius: 15
+  },
+  caixaBranca1: {
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    marginHorizontal: "10%",
+    borderRadius: 15,
   },
 
 });
